27e46b9e8f9a7afe81644093cafaf595aa24c380
Bohdan Kornatskyi <bohdan.kornatskyi@gmail.com>
2024-04-10 17:33:33 -0700

    split some classes into its own files

---
+++ include/Rendering/EntityRenderer.h
@@ -0,0 +1,90 @@
+#ifndef ENTITYRENDERER_HEADER
+#define ENTITYRENDERER_HEADER
+
+#include "../Environement/Carnivore.h"
+#include "../Environement/Entity.h"
+#include "../Environement/Herbivore.h"
+#include "../Environement/Resource.h"
+#include "../Physics/Physics.h"
+#include "../utils/math_utils.h"
+#include "../utils/utils.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
+
+class EntityRenderer {
+  public:
+  static sf::VertexArray createShape(const std::shared_ptr<Entity> &entity,
+                                     const sf::Color &color) {
+    switch (entity->getType()) {
+    case EntityType::RESOURCE:
+      return createRectangleShape(std::static_pointer_cast<Resource>(entity),
+                                  color);
+    default:
+      return createTriangleShape(entity, color);
+    }
+  }
+
+  static sf::Color determineColor(EntityType type) {
+    switch (type) {
+    case EntityType::CARNIVORE:
+      return sf::Color::Red;
+    case EntityType::HERBIVORE:
+      return sf::Color::Blue;
+    case EntityType::RESOURCE:
+      return sf::Color::Green;
+    default:
+      return sf::Color::White;
+    }
+  }
+
+  private:
+  static sf::VertexArray
+  createTriangleShape(const std::shared_ptr<Entity> &entity,
+                      const sf::Color &color) {
+    sf::VertexArray vertices(sf::Triangles, 3);
+    sf::Vector2f position = convertToSFMLCoordinate(entity->position);
+    std::array<sf::Vector2f, 3> points = {
+      rotatePointAround(position,
+                        sf::Vector2f(position.x - entity->radius / 2,
+                                     position.y + entity->radius / 2),
+                        -entity->angle),
+      rotatePointAround(position,
+                        sf::Vector2f(position.x - entity->radius / 2,
+                                     position.y - entity->radius / 2),
+                        -entity->angle),
+      rotatePointAround(position,
+                        sf::Vector2f(position.x + entity->radius, position.y),
+                        -entity->angle)};
+
+    for (int i = 0; i < 3; ++i) {
+      vertices[i].position = points[i];
+      vertices[i].color = color;
+    }
+
+    return vertices;
+  }
+
+  static sf::VertexArray
+  createRectangleShape(const std::shared_ptr<Resource> &entity,
+                       const sf::Color &color) {
+    sf::VertexArray vertices(sf::Quads, 4);
+    sf::Vector2f position = convertToSFMLCoordinate(entity->position);
+    float size =
+      entity
+        ->radius; // Assuming radius is used to determine the size of the square
+
+    vertices[0].position = position + sf::Vector2f(-size / 2, -size / 2);
+    vertices[1].position = position + sf::Vector2f(size / 2, -size / 2);
+    vertices[2].position = position + sf::Vector2f(size / 2, size / 2);
+    vertices[3].position = position + sf::Vector2f(-size / 2, size / 2);
+
+    for (int i = 0; i < 4; ++i) {
+      vertices[i].color = color;
+    }
+
+    return vertices;
+  }
+};
+
+#endif
--- include/Rendering/Rendering.h
+++ include/Rendering/Rendering.h
@@ -8,84 +8,11 @@
 #include "../Physics/Physics.h"
 #include "../utils/math_utils.h"
 #include "../utils/utils.h"
+#include "DrawablePhysics.h"
+#include "EntityRenderer.h"
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include <vector>
-
-class EntityRenderer {
-  public:
-  static sf::VertexArray createShape(const std::shared_ptr<Entity> &entity,
-                                     const sf::Color &color) {
-    switch (entity->getType()) {
-    case EntityType::RESOURCE:
-      return createRectangleShape(std::static_pointer_cast<Resource>(entity),
-                                  color);
-    default:
-      return createTriangleShape(entity, color);
-    }
-  }
-
-  static sf::Color determineColor(EntityType type) {
-    switch (type) {
-    case EntityType::CARNIVORE:
-      return sf::Color::Red;
-    case EntityType::HERBIVORE:
-      return sf::Color::Blue;
-    case EntityType::RESOURCE:
-      return sf::Color::Green;
-    default:
-      return sf::Color::White;
-    }
-  }
-
-  private:
-  static sf::VertexArray
-  createTriangleShape(const std::shared_ptr<Entity> &entity,
-                      const sf::Color &color) {
-    sf::VertexArray vertices(sf::Triangles, 3);
-    sf::Vector2f position = convertToSFMLCoordinate(entity->position);
-    std::array<sf::Vector2f, 3> points = {
-      rotatePointAround(position,
-                        sf::Vector2f(position.x - entity->radius / 2,
-                                     position.y + entity->radius / 2),
-                        -entity->angle),
-      rotatePointAround(position,
-                        sf::Vector2f(position.x - entity->radius / 2,
-                                     position.y - entity->radius / 2),
-                        -entity->angle),
-      rotatePointAround(position,
-                        sf::Vector2f(position.x + entity->radius, position.y),
-                        -entity->angle)};
-
-    for (int i = 0; i < 3; ++i) {
-      vertices[i].position = points[i];
-      vertices[i].color = color;
-    }
-
-    return vertices;
-  }
-
-  static sf::VertexArray
-  createRectangleShape(const std::shared_ptr<Resource> &entity,
-                       const sf::Color &color) {
-    sf::VertexArray vertices(sf::Quads, 4);
-    sf::Vector2f position = convertToSFMLCoordinate(entity->position);
-    float size =
-      entity
-        ->radius; // Assuming radius is used to determine the size of the square
-
-    vertices[0].position = position + sf::Vector2f(-size / 2, -size / 2);
-    vertices[1].position = position + sf::Vector2f(size / 2, -size / 2);
-    vertices[2].position = position + sf::Vector2f(size / 2, size / 2);
-    vertices[3].position = position + sf::Vector2f(-size / 2, size / 2);
-
-    for (int i = 0; i < 4; ++i) {
-      vertices[i].color = color;
-    }
-
-    return vertices;
-  }
-};
 
 class DrawableEntities : public sf::Drawable {
   private:
@@ -143,28 +70,6 @@ class DrawableEntities : public sf::Drawable {
 
     if (boundaryEnabled) {
       drawBoundaries(target, states);
-    }
-  }
-};
-
-class DrawablePhysics : public sf::Drawable {
-  public:
-  std::shared_ptr<Physics> physics;
-  DrawablePhysics(std::shared_ptr<Physics> physics) : physics(physics) {}
-
-  private:
-  protected:
-  virtual void draw(sf::RenderTarget &target,
-                    sf::RenderStates states) const override {
-    auto cells = physics->getCells();
-    for (auto &[x, y] : cells) {
-      auto rect = sf::RectangleShape(
-        sf::Vector2f(physics->getCellSize(), physics->getCellSize()));
-      rect.setPosition(x, y);
-      rect.setOutlineColor(sf::Color::Red);
-      rect.setFillColor(sf::Color::Transparent);
-      rect.setOutlineThickness(1);
-      target.draw(rect, states);
     }
   }
 };
