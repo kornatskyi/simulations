7fa1dfe95a44ef501240706262edbd5b91071f22
Bohdan Kornatskyi <bohdan.kornatskyi@gmail.com>
2025-01-04 19:05:05 -0800

    refactor createRectangleShape

--- include/Rendering/EntityRenderer.h
+++ include/Rendering/EntityRenderer.h
@@ -14,34 +14,35 @@
 #include "../utils/utils.h"
 
 class EntityRenderer {
- public:
+public:
   static sf::VertexArray createShape(const std::shared_ptr<Entity> &entity,
                                      const sf::Color &color) {
     switch (entity->getType()) {
-      case EntityType::RESOURCE:
-        return createRectangleShape(std::static_pointer_cast<Resource>(entity),
-                                    color);
-      default:
-        return createTriangleShape(entity, color);
+    case EntityType::RESOURCE:
+      return createRectangleShape(std::static_pointer_cast<Resource>(entity),
+                                  color);
+    default:
+      return createTriangleShape(entity, color);
     }
   }
 
   static sf::Color determineColor(EntityType type) {
     switch (type) {
-      case EntityType::CARNIVORE:
-        return sf::Color::Red;
-      case EntityType::HERBIVORE:
-        return sf::Color::Blue;
-      case EntityType::RESOURCE:
-        return sf::Color::Green;
-      default:
-        return sf::Color::White;
+    case EntityType::CARNIVORE:
+      return sf::Color::Red;
+    case EntityType::HERBIVORE:
+      return sf::Color::Blue;
+    case EntityType::RESOURCE:
+      return sf::Color::Green;
+    default:
+      return sf::Color::White;
     }
   }
 
- private:
-  static sf::VertexArray createTriangleShape(
-      const std::shared_ptr<Entity> &entity, const sf::Color &color) {
+private:
+  static sf::VertexArray
+  createTriangleShape(const std::shared_ptr<Entity> &entity,
+                      const sf::Color &color) {
     sf::VertexArray vertices(sf::PrimitiveType::Triangles, 3);
     sf::Vector2f position = convertToSFMLCoordinate(entity->position);
     std::array<sf::Vector2f, 3> points = {
@@ -65,21 +66,44 @@ class EntityRenderer {
     return vertices;
   }
 
-  static sf::VertexArray createRectangleShape(
-      const std::shared_ptr<Resource> &entity, const sf::Color &color) {
-    sf::VertexArray vertices(sf::PrimitiveType::LineStrip, 4);
+  static sf::VertexArray
+  createRectangleShape(const std::shared_ptr<Resource> &entity,
+                       const sf::Color &color) {
+
+    sf::VertexArray vertices(sf::PrimitiveType::Triangles);
+
+    sf::Vertex topLeft, bottomLeft, topRight, bottomRight;
+
     sf::Vector2f position = convertToSFMLCoordinate(entity->position);
-    float size = entity->radius;  // Assuming radius is used to determine the
-                                  // size of the square
 
-    vertices[0].position = position + sf::Vector2f(-size / 2, -size / 2);
-    vertices[1].position = position + sf::Vector2f(size / 2, -size / 2);
-    vertices[2].position = position + sf::Vector2f(size / 2, size / 2);
-    vertices[3].position = position + sf::Vector2f(-size / 2, size / 2);
+    float size = entity->radius; // Assuming radius is used to determine the
+                                 // size of the square
 
-    for (int i = 0; i < 4; ++i) {
-      vertices[i].color = color;
-    }
+    topLeft.position = sf::Vector2f({(position.x), (position.y)});
+
+    topRight.position = sf::Vector2f({(position.x) + size, (position.y)});
+
+    bottomLeft.position = sf::Vector2f({(position.x), (position.y) - size});
+
+    bottomRight.position =
+        sf::Vector2f({(position.x) + size, (position.y) - size});
+
+    topLeft.position = position + sf::Vector2f(-size / 2, -size / 2);
+    topRight.position = position + sf::Vector2f(size / 2, -size / 2);
+    bottomRight.position = position + sf::Vector2f(size / 2, size / 2);
+    bottomLeft.position = position + sf::Vector2f(-size / 2, size / 2);
+
+    topLeft.color = color;
+    topRight.color = color;
+    bottomLeft.color = color;
+    bottomRight.color = color;
+
+    vertices.append(topLeft);
+    vertices.append(bottomLeft);
+    vertices.append(topRight);
+    vertices.append(topRight);
+    vertices.append(bottomLeft);
+    vertices.append(bottomRight);
 
     return vertices;
   }
